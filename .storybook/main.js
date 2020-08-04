54d050a39db1210e75f85f0a94bc52f78f516891
Robert Watt <rwatt@four51.com>
2020-08-04 10:28:24 -0500

    Remove @storybook/addon-actions from .storybok/main.js
    Seems like we aren't using this tab anyway.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   stories: ['../projects/lib/src/stories/*.stories.ts'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-notes', '@storybook/addon-knobs/register'],
+  addons: ['@storybook/addon-links', '@storybook/addon-notes', '@storybook/addon-knobs/register'],
 };
